84c6db7ac75277ba7c11ab1ee8847eab271ba1e8
yoehuat <116549384+yoehuat@users.noreply.github.com>
2022-10-26 17:07:16 +0800

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,8 +1,8 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
-export PATH_vless=${PATH_vless-/vless/$UUID}
-export PATH_trojan=${PATH_trojan-/trojan/$UUID}
-export PATH_vmess=${PATH_vmess-/vmess/$UUID}
+export UUID=${UUID-79508ba07aaf4f938306ab451a3ff1f9}
+export PATH_vless=${PATH_vless-/vlss/$UUID}
+export PATH_trojan=${PATH_trojan-/trjan/$UUID}
+export PATH_vmess=${PATH_vmess-/vmss/$UUID}
 export PATH_shadowsocks=${PATH_shadowsocks-/shadowsocks/$UUID}
 
 tar -xzvf page.tar.gz
